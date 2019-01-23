d626957f3a1a0d3cb54fa23a795ca3c5d429dde7
jgrissom <jgrissom91@gmail.com>
2019-01-23 16:22:44 -0600

    Lesson-02 Assign

--- SleepData/Program.cs
+++ SleepData/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace SleepData
 {
@@ -58,8 +59,35 @@ namespace SleepData
             }
             else if (resp == "2")
             {
-                // TODO: parse data file
-
+                // parse data file
+                if (File.Exists(file))
+                {
+                    // read data from file
+                    StreamReader sr = new StreamReader(file);
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        // convert string to array
+                        // a line of text represents 1 week of data
+                        string[] week = line.Split(',');
+                        // the 1st element in the array is the date
+                        DateTime date = DateTime.Parse(week[0]);
+                        // the 2nd element is the hours of sleep with a "|" delimiter
+                        int[] hours = Array.ConvertAll(week[1].Split('|'), int.Parse);
+                        // display date for the current week
+                        Console.WriteLine($"Week of {date:MMM}, {date:dd}, {date:yyyy}");
+                        // display column headers
+                        Console.WriteLine($"{"Su",3}{"Mo",3}{"Tu",3}{"We",3}{"Th",3}{"Fr",3}{"Sa",3}"); // {"Tot",4}{"Avg",4}");
+                        Console.WriteLine($"{"--",3}{"--",3}{"--",3}{"--",3}{"--",3}{"--",3}{"--",3}"); // {"---",4}{"---",4}");
+                        // display hours of sleep for each day
+                        Console.WriteLine($"{hours[0],3}{hours[1],3}{hours[2],3}{hours[3],3}{hours[4],3}{hours[5],3}{hours[6],3}"); // {hours.Sum(),4}{Math.Round(hours.Sum() / 7.0, 1),4:n1}");
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("File does not exist");
+                }
             }
         }
     }
