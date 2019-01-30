e20b65a78eedee8c076eebae1df6cedca1508bb9
jgrissom <jgrissom91@gmail.com>
2019-01-30 12:48:26 -0600

    Configure NLog Targets for output
    1) Log all messages to the console
    2) Log Info, Warn, Error, & Fatal to log file

--- NLogExample/Program.cs
+++ NLogExample/Program.cs
@@ -7,7 +7,19 @@ namespace NLogExample
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // create NLog configuration
+            var config = new NLog.Config.LoggingConfiguration();
+
+            // define targets
+            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "log_file.txt" };
+            var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+
+            // specify minimum log level to maximum log level and target (console, file, etc.)
+            config.AddRule(LogLevel.Trace, LogLevel.Fatal, logconsole);
+            config.AddRule(LogLevel.Info, LogLevel.Fatal, logfile);
+
+            // apply NLog configuration
+            NLog.LogManager.Configuration = config;
         }
     }
 }
