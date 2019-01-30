05d15dff72692af934afdb385cc04f92a4014f81
jgrissom <jgrissom91@gmail.com>
2019-01-30 09:10:08 -0600

    NLog Example
    1) Install NLog package on project
    2) Import NLog namespace into class

---
+++ NLogExample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NLog;
+
+namespace NLogExample
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
