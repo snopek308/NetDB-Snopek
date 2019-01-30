b29739b3f9d658ca726ebbc6719688b1c9459cb1
jgrissom <jgrissom91@gmail.com>
2019-01-30 12:59:14 -0600

    Create instance of LogManager & create sample log messages for all log levels

--- NLogExample/Program.cs
+++ NLogExample/Program.cs
@@ -20,6 +20,17 @@ namespace NLogExample
 
             // apply NLog configuration
             NLog.LogManager.Configuration = config;
+
+            // create instance of LogManager
+            var logger = NLog.LogManager.GetCurrentClassLogger();
+
+            // log sample messages
+            logger.Trace("Sample trace message");
+            logger.Debug("Sample debug message");
+            logger.Info("Sample informational message");
+            logger.Warn("Sample warning message");
+            logger.Error("Sample error message");
+            logger.Fatal("Sample fatal error message");
         }
     }
 }
