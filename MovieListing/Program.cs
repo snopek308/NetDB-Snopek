ef0be73ac57e5508475762a9af1ce9ccb45da688
jgrissom <jgrissom91@gmail.com>
2019-02-01 11:03:42 -0600

    Create MovieListing Application
    1) Add new project to solution (MovieListing)
    2) Copy movie CSV files to project data folder
    3) Add NLog package
    4) Create NLog.config file (it needs to be copied to the bin/debug folder) filename is case-sensitive
    5) Import NLog namespace into program.cs
    6) Create class-level logger
    7) Log program start/end

---
+++ MovieListing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NLog;
+
+namespace MovieListing
+{
+    class MainClass
+    {
+        // create a class level instance of logger (can be used in methods other than Main)
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        public static void Main(string[] args)
+        {
+            // config is loaded using xml (NLog.config saved in  debug folder)
+            logger.Info("Program started");
+
+            logger.Info("Program ended");
+        }
+    }
+}
