6b6381c40e5ba4985adb676a07b29f1391e7dbe4
jgrissom <jgrissom91@gmail.com>
2019-02-01 18:42:12 -0600

    Setup user choices
    1) Make sure data file exists
    2) Display choices in a loop

--- MovieListing/Program.cs
+++ MovieListing/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using NLog;
 
 namespace MovieListing
@@ -11,6 +12,39 @@ namespace MovieListing
         {
             // config is loaded using xml (NLog.config saved in  debug folder)
             logger.Info("Program started");
+
+            // path to movie data file
+            string file = "../../data/movies.csv";
+
+            // make sure movie file exists
+            if (!File.Exists(file))
+            {
+                logger.Error("File does not exist: {File}", file);
+            }
+            else
+            {
+                string choice;
+                do
+                {
+                    // display choices to user
+                    Console.WriteLine("1) Add Movie");
+                    Console.WriteLine("2) Display All Movies");
+                    Console.WriteLine("Enter to quit");
+
+                    // input selection
+                    choice = Console.ReadLine();
+                    logger.Info("User choice: {Choice}", choice);
+
+                    if (choice == "1")
+                    {
+                        // Add Movie
+                    }
+                    else if (choice == "2")
+                    {
+                        // Display All Movies
+                    }
+                } while (choice == "1" || choice == "2");
+            }
 
             logger.Info("Program ended");
         }
