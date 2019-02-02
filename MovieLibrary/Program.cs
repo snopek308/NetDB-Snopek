77aab32e37b6bad01f5c45093e83e7f1394048a4
jgrissom <jgrissom91@gmail.com>
2019-02-02 10:56:47 -0600

    Input Movie Title
    1) Ask user to enter movie title
    2) Verify the title is unique

--- MovieLibrary/Program.cs
+++ MovieLibrary/Program.cs
@@ -28,6 +28,20 @@ namespace MovieLibrary
                 if (choice == "1")
                 {
                     // Add movie
+                    Movie movie = new Movie();
+                    // ask user to input movie title
+                    Console.WriteLine("Enter movie title");
+                    // input title
+                    movie.title = Console.ReadLine();
+                    // verify title is unique
+                    if (movieFile.isUniqueTitle(movie.title)){
+                        Console.WriteLine("Movie title is unique\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Movie title already exists\n");
+                    }
+
                 } else if (choice == "2")
                 {
                     // Display All Movies
