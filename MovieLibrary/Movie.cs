3599a0ed838639d0c223c6d6c49edaef1f788d57
jgrissom <jgrissom91@gmail.com>
2019-02-02 09:20:42 -0600

    Create Movie class
    1) Add movieId property
    2) Add title property
    3) Add genres property

---
+++ MovieLibrary/Movie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieLibrary
+{
+    public class Movie
+    {
+        public UInt64 movieId { get; set; }
+        public string title { get; set; }
+        public List<string> genres { get; set; }
+    }
+}
