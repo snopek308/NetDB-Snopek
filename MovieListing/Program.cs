bfb761bfeb577ac6c414167bbf4e301ffddf6f82
jgrissom <jgrissom91@gmail.com>
2019-02-01 20:44:07 -0600

    Save data to file
    1) OOPS! I forgot to close the file after reading - this resulted in the file being locked!
    2) Open StreamWriter for append - you don’t want to overwrite the file!
    3) Add movie details to Lists

--- MovieListing/Program.cs
+++ MovieListing/Program.cs
@@ -84,6 +84,8 @@ namespace MovieListing
                                 MovieGenres.Add(line.Replace("|", ", "));
                             }
                         }
+                        // close file when done
+                        sr.Close();
                     }
                     catch (Exception ex)
                     {
@@ -135,7 +137,15 @@ namespace MovieListing
                             // if there is a comma(,) in the title, wrap it in quotes
                             movieTitle = movieTitle.IndexOf(',') != -1 ? $"\"{movieTitle}\"" : movieTitle;
                             // display movie id, title, genres
-                            Console.WriteLine($"{movieId},{movieTitle},{genresString}");
+                            //Console.WriteLine($"{movieId},{movieTitle},{genresString}");
+                            // create file from data
+                            StreamWriter sw = new StreamWriter(file, true);
+                            sw.WriteLine($"{movieId},{movieTitle},{genresString}");
+                            sw.Close();
+                            // add movie details to Lists
+                            MovieIds.Add(movieId);
+                            MovieTitles.Add(movieTitle);
+                            MovieGenres.Add(genresString);
                         }
                     }
                     else if (choice == "2")
