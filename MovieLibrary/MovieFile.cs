4e976b1e5105b212a4764f69c85f7ba11c9761ea
jgrissom <jgrissom91@gmail.com>
2019-02-02 10:29:33 -0600

    Update MovieFile class
    1) Add logger
    2) Add Movies List property
    3) Populate Movies List from data file in class constructor

--- MovieLibrary/MovieFile.cs
+++ MovieLibrary/MovieFile.cs
@@ -1,18 +1,74 @@
 ﻿using NLog;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace MovieLibrary
 {
     public class MovieFile
     {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
         // public property
         public string filePath { get; set; }
+        public List<Movie> Movies { get; set; }
 
         // constructor is a special method that is invoked
         // when an instance of a class is created
         public MovieFile(string path)
         {
+            Movies = new List<Movie>();
             filePath = path;
+            // to populate the list with data, read from the data file
+            try
+            {
+                StreamReader sr = new StreamReader(filePath);
+                // first line contains column headers
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    // create instance of Movie class
+                    Movie movie = new Movie();
+                    string line = sr.ReadLine();
+                    // first look for quote(") in string
+                    // this indicates a comma(,) in movie title
+                    int idx = line.IndexOf('"');
+                    if (idx == -1)
+                    {
+                        // no quote = no comma in movie title
+                        // movie details are separated with comma(,)
+                        string[] movieDetails = line.Split(',');
+                        movie.movieId = UInt64.Parse(movieDetails[0]);
+                        movie.title = movieDetails[1];
+                        movie.genres = movieDetails[2].Split('|').ToList();
+                    }
+                    else
+                    {
+                        // quote = comma in movie title
+                        // extract the movieId
+                        movie.movieId = UInt64.Parse(line.Substring(0, idx - 1));
+                        // remove movieId and first quote from string
+                        line = line.Substring(idx + 1);
+                        // find the next quote
+                        idx = line.IndexOf('"');
+                        // extract the movieTitle
+                        movie.title = line.Substring(0, idx);
+                        // remove title and last comma from the string
+                        line = line.Substring(idx + 2);
+                        // replace the "|" with ", "
+                        movie.genres = line.Split('|').ToList();
+                    }
+                    Movies.Add(movie);
+                }
+                // close file when done
+                sr.Close();
+                logger.Info("Movies in file {Count}", Movies.Count);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
         }
     }
 }
