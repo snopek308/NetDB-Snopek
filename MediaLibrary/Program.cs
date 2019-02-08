dfaae25c8f76ed379d61ad69f1936e465cf0f0d7
jgrissom <jgrissom91@gmail.com>
2019-02-08 16:14:52 -0600

    Create Media Library Program
    1) Create new Program
    2) Copy movies.csv
    3) Add NLog package
    4) Copy Blog.config

---
+++ MediaLibrary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NLog;
+
+namespace MediaLibrary
+{
+    class MainClass
+    {
+        // create a class level instance of logger (can be used in methods other than Main)
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        public static void Main(string[] args)
+        {
+            logger.Info("Program started");
+
+            logger.Info("Program ended");
+        }
+    }
+}
