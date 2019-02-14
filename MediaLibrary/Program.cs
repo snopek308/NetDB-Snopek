07f34cb774c4ff39f022b00c5cf708df94b62d04
jgrissom <jgrissom91@gmail.com>
2019-02-14 15:20:55 -0600

    Create File Scrubber
    1) Create Static class / method for scrubbing the movies.csv file
    2) Update Program class to utilize the static method to scrub the file

--- MediaLibrary/Program.cs
+++ MediaLibrary/Program.cs
@@ -12,38 +12,7 @@ namespace MediaLibrary
         {
             logger.Info("Program started");
 
-            Movie movie = new Movie
-            {
-                mediaId = 123,
-                title = "Greatest Movie Ever, The (2019)",
-                director = "Jeff Grissom",
-                // timespan (hours, minutes, seconds)
-                runningTime = new TimeSpan(2, 21, 23),
-                genres = { "Comedy", "Romance" }
-            };
-
-            Console.WriteLine(movie.Display());
-
-            Album album = new Album
-            {
-                mediaId = 321,
-                title = "Greatest Album Ever, The (2019)",
-                artist = "Jeff's Awesome Band",
-                recordLabel = "Universal Music Group",
-                genres = { "Rock" }
-            };
-            Console.WriteLine(album.Display());
-
-            Book book = new Book
-            {
-                mediaId = 111,
-                title = "Super Cool Book",
-                author = "Jeff Grissom",
-                pageCount = 101,
-                publisher = "",
-                genres = { "Suspense", "Mystery" }
-            };
-            Console.WriteLine(book.Display());
+            string scrubbedFile = FileScrubber.ScrubMovies("../../movies.csv");
 
             logger.Info("Program ended");
         }
