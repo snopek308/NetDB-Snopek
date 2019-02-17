f07a8255c9ed2814a381963618ede0647ba4e9d4
jgrissom <jgrissom91@gmail.com>
2019-02-16 19:17:07 -0600

    Album File
    1) Create AlbumFile Class
    2) Create methods to Add an album and Display all albums
    3) Modify Program class - create menu to manage albums (Add, Display)

---
+++ MediaLibrary/AlbumFile.cs
@@ -0,0 +1,119 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MediaLibrary
+{
+    public class AlbumFile
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        // public property
+        public string filePath { get; set; }
+        public List<Album> Albums { get; set; }
+
+        public AlbumFile(string path)
+        {
+            Albums = new List<Album>();
+            filePath = path;
+            // to populate the list with data, read from the data file
+            try
+            {
+                if (File.Exists(path))
+                {
+                    StreamReader sr = new StreamReader(filePath);
+                    while (!sr.EndOfStream)
+                    {
+                        // create instance of Album class
+                        Album album = new Album();
+                        string line = sr.ReadLine();
+                        // first look for quote(") in string
+                        // this indicates a comma(,) in album title
+                        int idx = line.IndexOf('"');
+                        if (idx == -1)
+                        {
+                            // no quote = no comma in album title
+                            // album details are separated with comma(,)
+                            string[] albumDetails = line.Split(',');
+                            album.mediaId = UInt64.Parse(albumDetails[0]);
+                            album.title = albumDetails[1];
+                            album.genres = albumDetails[2].Split('|').ToList();
+                            album.artist = albumDetails[3];
+                            album.recordLabel = albumDetails[4];
+                        }
+                        else
+                        {
+                            // quote = comma or quotes in album title
+                            // extract the albumId
+                            album.mediaId = UInt64.Parse(line.Substring(0, idx - 1));
+                            // remove albumId and first comma from string
+                            line = line.Substring(idx);
+                            // find the last quote
+                            idx = line.LastIndexOf('"');
+                            // extract title
+                            album.title = line.Substring(0, idx + 1);
+                            // remove title and next comma from the string
+                            line = line.Substring(idx + 2);
+                            // split the remaining string based on commas
+                            string[] details = line.Split(',');
+                            // the first item in the array should be genres 
+                            album.genres = details[0].Split('|').ToList();
+                            // the next item in the array should be artist
+                            album.artist = details[1];
+                            // the next item in the array should be record label
+                            album.recordLabel = details[2];
+                        }
+                        Albums.Add(album);
+                    }
+                    // close file when done
+                    sr.Close();
+                    logger.Info("Albums in file {Count}", Albums.Count);
+                }
+                else
+                {
+                    logger.Info("The file does not exist {Path}", path);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
+        }
+
+        // public method
+        public bool isUniqueTitle(string title)
+        {
+            if (Albums.ConvertAll(a => a.title.ToLower()).Contains(title.ToLower()))
+            {
+                logger.Info("Duplicate album title {Title}", title);
+                return false;
+            }
+            return true;
+        }
+
+        public void AddAlbum(Album album)
+        {
+            try
+            {
+                // first generate album id
+                album.mediaId = Albums.Count == 0 ? 1 : Albums.Max(a => a.mediaId) + 1;
+                // if title contains a comma, wrap it in quotes
+                string title = album.title.IndexOf(',') != -1 || album.title.IndexOf('"') != -1 ? $"\"{album.title}\"" : album.title;
+                StreamWriter sw = new StreamWriter(filePath, true);
+                // write album data to file
+                sw.WriteLine($"{album.mediaId},{title},{string.Join("|", album.genres)},{album.artist},{album.recordLabel}");
+                sw.Close();
+                // add album details to List
+                Albums.Add(album);
+                // log transaction
+                logger.Info("Media id {Id} added", album.mediaId);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
+        }
+    }
+}
--- MediaLibrary/Program.cs
+++ MediaLibrary/Program.cs
@@ -13,7 +13,9 @@ namespace MediaLibrary
             logger.Info("Program started");
 
             string scrubbedFile = FileScrubber.ScrubMovies("../../movies.csv");
+            string albumFileName = "../../albums.csv";
             MovieFile movieFile = new MovieFile(scrubbedFile);
+            AlbumFile albumFile = new AlbumFile(albumFileName);
 
             string choice = "";
             do
@@ -21,6 +23,10 @@ namespace MediaLibrary
                 // display choices to user
                 Console.WriteLine("1) Add Movie");
                 Console.WriteLine("2) Display All Movies");
+                Console.WriteLine("3) Add Album");
+                Console.WriteLine("4) Display All Albums");
+                Console.WriteLine("5) Add Book");
+                Console.WriteLine("6) Display All Books");
                 Console.WriteLine("Enter to quit");
                 // input selection
                 choice = Console.ReadLine();
@@ -81,7 +87,62 @@ namespace MediaLibrary
                         Console.WriteLine(m.Display());
                     }
                 }
-            } while (choice == "1" || choice == "2");
+                else if (choice == "3")
+                {
+                    // Add Album
+                    Album album = new Album();
+                    // ask user to input album title
+                    Console.WriteLine("Enter album title");
+                    // input title
+                    album.title = Console.ReadLine();
+                    // verify title is unique
+                    if (albumFile.isUniqueTitle(album.title))
+                    {
+                        // input genres
+                        string input;
+                        do
+                        {
+                            // ask user to enter genre
+                            Console.WriteLine("Enter genre (or done to quit)");
+                            // input genre
+                            input = Console.ReadLine();
+                            // if user enters "done"
+                            // or does not enter a genre do not add it to list
+                            if (input != "done" && input.Length > 0)
+                            {
+                                album.genres.Add(input);
+                            }
+                        } while (input != "done");
+                        // specify if no genres are entered
+                        if (album.genres.Count == 0)
+                        {
+                            album.genres.Add("(no genres listed)");
+                        }
+                        // ask user to enter director
+                        Console.WriteLine("Enter album artist");
+                        input = Console.ReadLine();
+                        album.artist = input.Length == 0 ? "unassigned" : input;
+                        // ask user to enter record label
+                        Console.WriteLine("Enter record label");
+                        input = Console.ReadLine();
+                        album.recordLabel = input.Length == 0 ? "unassigned" : input;
+                        // add album
+                        albumFile.AddAlbum(album);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Album title already exists\n");
+                    }
+                }
+                else if (choice == "4")
+                {
+                    // Display All Albums
+                    foreach (Album a in albumFile.Albums)
+                    {
+                        Console.WriteLine(a.Display());
+                    }
+                }
+            } while (choice == "1" || choice == "2" || choice == "3" || choice == "4");
 
             logger.Info("Program ended");
         }
